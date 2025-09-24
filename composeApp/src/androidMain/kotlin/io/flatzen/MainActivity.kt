8b4a5b0105d3fb6f8bf18c056bfb38bf660af8db
Denis.Gonchar <denisgonchar99@gmail.com>
2025-09-24 17:52:56 +0300

    add splashScreen icon

--- composeApp/src/androidMain/kotlin/io/flatzen/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/io/flatzen/MainActivity.kt
@@ -7,20 +7,27 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.flatzen.viewmodel.SplashUiState
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
     val splashScreenViewModel by viewModel<SplashScreenViewModel>()
-    var isSplashVisible = true
+    private var isSplashVisible by mutableStateOf(true)
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         val splashScreen = installSplashScreen()
         
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        
+        splashScreen.setKeepOnScreenCondition {
+            isSplashVisible
+        }
+        
         setContent {
             // Remove when https://issuetracker.google.com/issues/364713509 is fixed
             LaunchedEffect(isSystemInDarkTheme()) {
@@ -28,12 +35,10 @@ class MainActivity : ComponentActivity() {
             }
             LaunchedEffect(Unit) {
                 splashScreenViewModel.uiState.collect {
-                    isSplashVisible = it != SplashUiState.Loading
+                    isSplashVisible = it is SplashUiState.Loading
                 }
             }
-            splashScreen.setKeepOnScreenCondition {
-                isSplashVisible
-            }
+            
             App()
         }
     }
