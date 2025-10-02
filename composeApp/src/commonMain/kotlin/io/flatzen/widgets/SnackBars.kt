b96e8f07f60f570efc608d67703a7f1bf6a75e05
Denis.Gonchar <denisgonchar99@gmail.com>
2025-10-02 20:06:35 +0300

    Add network snackbar

---
+++ composeApp/src/commonMain/kotlin/io/flatzen/widgets/SnackBars.kt
@@ -0,0 +1,32 @@
+package io.flatzen.widgets
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ErrorSnackbar(
+    message: String,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp)
+            .background(Color.Red),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            text = message,
+            color = Color.White,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+    }
+}
