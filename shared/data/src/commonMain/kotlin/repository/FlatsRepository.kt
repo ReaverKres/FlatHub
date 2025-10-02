b91fd351b459d14c6fc05e11fe95f6b8dde392e5
Denis.Gonchar <denisgonchar99@gmail.com>
2025-10-02 11:21:50 +0300

    Repository getFlatById Updated

--- shared/data/src/commonMain/kotlin/repository/FlatsRepository.kt
+++ shared/data/src/commonMain/kotlin/repository/FlatsRepository.kt
@@ -1,9 +1,12 @@
 package repository
 
 import core.NetworkResponseWrapper
+import database.FlatsDao
 import entities.AppFlat
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 
 interface FlatsRepository {
 
@@ -13,5 +16,19 @@ interface FlatsRepository {
         flatId: Long
     ): Flow<AppFlat>
 
+    fun getFlatByIdWithDetails(
+        flatId: Long
+    ): Flow<AppFlat?>
+
     fun clearCashedFlats()
+}
+
+internal suspend fun FlowCollector<AppFlat>.getFlatByIdFromDb(flatId: Long, flatsDao: FlatsDao): AppFlat {
+    val flatFromList = flatsDao.getAllAsFlow()
+        .map { flats ->
+            flats.find { it.adId == flatId }
+                ?: throw NoSuchElementException("Flat with id $flatId not found")
+        }.first()
+    emit(flatFromList)
+    return flatFromList
 }
