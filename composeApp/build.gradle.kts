97442e60e740906c97edce382edfe734887f8c68
Denis.Gonchar <denisgonchar99@gmail.com>
2025-10-25 13:04:22 +0300

    release version 1.0.2

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -86,8 +86,8 @@ android {
         applicationId = "io.flatzen"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     signingConfigs {
