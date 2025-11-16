758074f94bc03de6eeeb7ea7c38fda00562320f8
Denis.Gonchar <denisgonchar99@gmail.com>
2025-11-16 18:33:31 +0300

    update room for ios

--- shared/data/src/iosMain/kotlin/io/flatzen/database/IosDatabase.kt
+++ shared/data/src/iosMain/kotlin/io/flatzen/database/IosDatabase.kt
@@ -4,13 +4,28 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import database.AppDatabase
 import database.getRoomDatabase
-import platform.Foundation.NSHomeDirectory
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSUserDomainMask
 
 fun getDatabaseBuilder(): RoomDatabase.Builder<AppDatabase> {
-    val dbFilePath = NSHomeDirectory() + "/my_room.db"
+    val dbFilePath = documentDirectory() + "/my_room.db"
     return Room.databaseBuilder<AppDatabase>(
-        name = dbFilePath
+        name = dbFilePath,
     )
+}
+
+@OptIn(ExperimentalForeignApi::class)
+private fun documentDirectory(): String {
+    val documentDirectory = NSFileManager.defaultManager.URLForDirectory(
+        directory = NSDocumentDirectory,
+        inDomain = NSUserDomainMask,
+        appropriateForURL = null,
+        create = false,
+        error = null,
+    )
+    return requireNotNull(documentDirectory?.path)
 }
 
 fun getDatabase(): AppDatabase {
