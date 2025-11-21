4887c6cfbb0c05bff8dce0a0d58ceea6b6032802
Denis.Gonchar <denisgonchar99@gmail.com>
2025-11-21 11:09:10 +0300

    fix share button on ipad

--- composeApp/src/iosMain/kotlin/io/flatzen/utils/ShareLauncher.ios.kt
+++ composeApp/src/iosMain/kotlin/io/flatzen/utils/ShareLauncher.ios.kt
@@ -2,8 +2,13 @@ package io.flatzen.utils
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.CoreGraphics.CGRectGetMidX
+import platform.CoreGraphics.CGRectGetMidY
+import platform.CoreGraphics.CGRectMake
 import platform.UIKit.UIActivityViewController
 import platform.UIKit.UIApplication
+import platform.UIKit.popoverPresentationController
 
 @Composable
 actual fun shareLauncher(onShareError: (Exception) -> Unit): ShareLauncher {
@@ -15,6 +20,7 @@ actual fun shareLauncher(onShareError: (Exception) -> Unit): ShareLauncher {
 class IOSShareLauncher(
     private val onShareError: (exception: Exception) -> Unit
 ) : ShareLauncher {
+    @OptIn(ExperimentalForeignApi::class)
     override fun shareText(text: String, subject: String) {
         try {
             val items = mutableListOf<Any>()
@@ -30,6 +36,17 @@ class IOSShareLauncher(
             
             val rootViewController = UIApplication.sharedApplication.keyWindow?.rootViewController
             val presentedViewController = rootViewController?.presentedViewController ?: rootViewController
+            
+            activityViewController.popoverPresentationController?.let { popover ->
+                val view = presentedViewController?.view
+                if (view != null) {
+                    popover.sourceView = view
+                    val bounds = view.bounds
+                    val x = CGRectGetMidX(bounds)
+                    val y = CGRectGetMidY(bounds)
+                    popover.sourceRect = CGRectMake(x, y, 1.0, 1.0)
+                }
+            }
             
             presentedViewController?.presentViewController(
                 viewControllerToPresent = activityViewController,
