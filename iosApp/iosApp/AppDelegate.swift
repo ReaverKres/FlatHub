b272cfad8f0d44a3390649d9ef47e7dc8565014b
Denis.Gonchar <denisgonchar99@gmail.com>
2025-11-26 20:14:58 +0300

    notification ios

---
+++ iosApp/iosApp/AppDelegate.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        return true
+    }
+
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) async -> UIBackgroundFetchResult {
+        NotifierManager.shared.onApplicationDidReceiveRemoteNotification(userInfo: userInfo)
+        return UIBackgroundFetchResult.newData
+    }
+}
